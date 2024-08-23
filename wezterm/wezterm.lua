cfa1dd1a6094401dd794900e72a8e76b8c565759
Szukyu <vermillionbirdszukyu@gmail.com>
2024-08-23 18:53:32 -0400

    Add files via upload

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,14 +3,34 @@ local mux = wezterm.mux
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+config.enable_wayland = true
+config.webgpu_power_preference = "HighPerformance"
+-- config.animation_fps = 1
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+-- Colorscheme
+config.color_scheme_dirs = { wezterm.home_dir .. "/projects/tokyonight.nvim/extras/wezterm" }
+config.color_scheme = "tokyonight_night"
+wezterm.add_to_config_reload_watch_list(config.color_scheme_dirs[1] .. config.color_scheme .. ".toml")
+
+config.colors = {
+  indexed = { [241] = "#65bcff" },
+}
+
+config.underline_thickness = 3
+config.cursor_thickness = 4
+config.underline_position = -6
+
 config.font_size = 19
+config.font = wezterm.font({ family = "Fira Code" })
+config.bold_brightens_ansi_colors = true
+
+config.default_cursor_style = "BlinkingBar"
+config.force_reverse_video_cursor = true
+config.scrollback_lines = 10000
 
 config.enable_tab_bar = false
-
-config.scrollback_lines = 3000
 
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
