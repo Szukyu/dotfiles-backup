601f4580c5a1000019ce08bf877677d44e98b414
Szukyu <vermillionbirdszukyu@gmail.com>
2024-08-27 20:38:16 -0400

    Delete sketchybar directory

--- sketchybar/plugins/cpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CPU=$(top -l 1 | awk '/^CPU usage:/ {print $3}' | tr -d '%' | cut -d "." -f1)
-
-sketchybar --set $NAME \
-  icon="󰍛" \
-  icon.color=0xFFb4befe \
-  label="$CPU%"
-
--- sketchybar/plugins/ram.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RAM=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')
-
-sketchybar --set $NAME \
-  icon= \
-  icon.color=0xfffab387 \
-  label="$RAM%"
