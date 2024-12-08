09d1497070d12cc31d207ff0817f1b4aacd0ecde
Szukyu <vermillionbirdszukyu@gmail.com>
2024-12-08 00:50:04 -0500

    Add files via upload

---
+++ nvim/lua/plugins/presence.lua
@@ -0,0 +1,24 @@
+return {
+  "andweeb/presence.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("presence").setup{
+      auto_update = true,
+      neovim_image_text = "The Superior Text Editor",
+      main_image = "file",
+      client_id = "793271441293967371",
+      log_level = nil,
+      debounce_timeout = 10,
+      blacklist = {},
+      file_assest = {},
+      enable_line_number = false,
+      show_time = true,
+
+      editing_text = "Editing %s",
+      file_explorer_text = "Browsing %s",
+      git_commit_text = "Commiting Changes",
+      workspace_text      = "Working on %s",
+      plugin_manager_text = "Managing Plugins",
+    }
+  end,
+}
