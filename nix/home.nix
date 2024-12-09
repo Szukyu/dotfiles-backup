e4f4e0bc7bee03ed7d28d9c40007e7c6b238cbe0
Szukyu <vermillionbirdszukyu@gmail.com>
2024-12-09 13:15:52 -0500

    Add files via upload

---
+++ nix/home.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+in
+{
+  programs.home-manager.enable = true;
+
+  home.username = "szukyu";
+  home.homeDirectory = "/Users/szukyu";
+  xdg.enable = true;
+
+	xdg.configFile.nvim.source = mkOutOfStoreSymlink "/Users/szukyu/.dotfiles/nvim";
+
+  home.stateVersion = "23.11";
+}
